6fdde7667ada908ad84956e27522dd2b2b755357
Pey-crypto <53893419+Pey-crypto@users.noreply.github.com>
2020-11-26 15:00:12 +0530

    Slight fixes

--- webp/webp/magic.sh
+++
@@ -1,8 +0,0 @@
-emcc -O3 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s  EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' \
-    -I libwebp \
-    webp.c \
-    libwebp/src/{dec,dsp,demux,enc,mux,utils}/*.c
-
-python -m http.server
-
-
